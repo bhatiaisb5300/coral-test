126a9213a66ed0fb4c5759e0e79117e3d9d57811
Ishneet Bhatia <52737993+bhatiaisb5300@users.noreply.github.com>
2021-03-08 00:31:15 +0530

    Update inference_tflite.py

--- inference_tflite.py
+++ inference_tflite.py
@@ -3,6 +3,7 @@ import datetime
 from tflite_runtime.interpreter import Interpreter 
 from tflite_runtime.interpreter import load_delegate 
 from PIL import Image
+import numpy as np
 #def preprocess(img):
 #    img = cv2.resize(img, (1344,448))
 #    return (img).reshape(1,1344,448,3).astype(np.uint8)
@@ -22,7 +23,7 @@ interpreter.allocate_tensors()
 input_index = interpreter.get_input_details()[0]["index"]
 output_index = interpreter.get_output_details()[0]["index"]
 
-img = image.reshape(1,1344,448,3).astype(np.uint8)
+img = np.array(image).reshape(1,1344,448,3).astype(np.uint8)
 
 interpreter.allocate_tensors()
 interpreter.set_tensor(input_index, img)
