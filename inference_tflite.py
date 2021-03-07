491ad107ef038e06f875776347296a5945433623
Ishneet Bhatia <52737993+bhatiaisb5300@users.noreply.github.com>
2021-03-08 00:26:51 +0530

    Update inference_tflite.py

--- inference_tflite.py
+++ inference_tflite.py
@@ -2,15 +2,15 @@
 import datetime
 from tflite_runtime.interpreter import Interpreter 
 from tflite_runtime.interpreter import load_delegate 
-
-def preprocess(img):
-    img = cv2.resize(img, (1344,448))
-    return (img).reshape(1,1344,448,3).astype(np.uint8)
+from PIL import Image
+#def preprocess(img):
+#    img = cv2.resize(img, (1344,448))
+#    return (img).reshape(1,1344,448,3).astype(np.uint8)
 
 img_path = 'IMG_20210201_171916.jpg'
-img = cv2.imread(img_path)
-img = preprocess(img)
-
+#img = cv2.imread(img_path)
+#img = preprocess(img)
+image = Image.open(img_path).convert('RGB').resize((1344,448), Image.ANTIALIAS)
 path = 'converted_model_quant_edgetpu.tflite'
 # interpreter = tf.lite.Interpreter(model_content=tflite_model)
 interpreter = Interpreter(model_path=path, experimental_delegates=[load_delegate('libedgetpu.so.1')])
